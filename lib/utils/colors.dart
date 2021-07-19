f5046230da907e9c43156d23c591bf2bf3b92470
Sachin Singh <sachins@geekyants.com>
2021-07-19 16:26:50 +0530

    sizeconfig and color constant files added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color orange = Color(0xFFF2623E);
+  static const purple = Color(0xFF58419C);
+  static const darkBlue = Color(0xFF0D1F44);
+}
