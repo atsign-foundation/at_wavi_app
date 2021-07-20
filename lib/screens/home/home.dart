913871fc3db1c13bff7171f5cb8b90aa77e92ecf
Sachin Singh <sachins@geekyants.com>
2021-07-20 17:24:15 +0530

    text color updated on dark mode

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -145,14 +145,18 @@ class _HomeScreenState extends State<HomeScreen> {
                                       '0',
                                       style: TextStyle(
                                           fontSize: 18.toFont,
-                                          color: _themeData.highlightColor,
+                                          color: _isDark
+                                              ? Colors.white
+                                              : _themeData.highlightColor,
                                           fontWeight: FontWeight.w800),
                                     ),
                                     Text(
                                       'Followers',
                                       style: TextStyle(
                                           fontSize: 14.toFont,
-                                          color: _themeData.highlightColor),
+                                          color: _isDark
+                                              ? Colors.white.withOpacity(0.5)
+                                              : Colors.black.withOpacity(0.5)),
                                     ),
                                   ],
                                 ),
@@ -166,14 +170,18 @@ class _HomeScreenState extends State<HomeScreen> {
                                       '0',
                                       style: TextStyle(
                                           fontSize: 18.toFont,
-                                          color: _themeData.highlightColor,
+                                          color: _isDark
+                                              ? Colors.white
+                                              : _themeData.highlightColor,
                                           fontWeight: FontWeight.w800),
                                     ),
                                     Text(
                                       'Following',
                                       style: TextStyle(
                                           fontSize: 14.toFont,
-                                          color: _themeData.highlightColor),
+                                          color: _isDark
+                                              ? Colors.white.withOpacity(0.5)
+                                              : Colors.black.withOpacity(0.5)),
                                     ),
                                   ],
                                 ),
@@ -201,7 +209,9 @@ class _HomeScreenState extends State<HomeScreen> {
                             'Edit Profile',
                             style: TextStyle(
                                 fontSize: 16.toFont,
-                                color: _themeData.highlightColor),
+                                color: _isDark
+                                    ? Colors.white.withOpacity(0.5)
+                                    : Colors.black.withOpacity(0.5)),
                           ),
                         ),
                       ),
@@ -219,7 +229,9 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Text('Share Profile',
                               style: TextStyle(
                                   fontSize: 16.toFont,
-                                  color: _themeData.highlightColor)),
+                                  color: _isDark
+                                      ? Colors.white.withOpacity(0.5)
+                                      : Colors.black.withOpacity(0.5))),
                         ),
                       ),
                     )
