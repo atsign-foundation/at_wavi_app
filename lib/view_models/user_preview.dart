bd0f2683b58ab93366a7875f900137ae69f1fa68
Sachin Singh <sachins@geekyants.com>
2021-08-04 13:44:47 +0530

    user model updated and preview user file added

---
+++ lib/view_models/user_preview.dart
@@ -0,0 +1,22 @@
+import 'package:at_wavi_app/model/user.dart';
+import 'package:at_wavi_app/view_models/base_model.dart';
+
+class UserPreview extends BaseModel {
+  UserPreview._();
+  static UserPreview _instance = UserPreview._();
+  factory UserPreview() => _instance;
+
+  User? _user;
+
+  set setUser(User? user) {
+    this._user = user;
+  }
+
+  User? user() {
+    return _user;
+  }
+
+  void notify() {
+    notifyListeners();
+  }
+}
