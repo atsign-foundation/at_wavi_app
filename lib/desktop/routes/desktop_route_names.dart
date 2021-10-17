4a983247c3c00010062776478eba31ad70fce915
sonle <sonle@geekyants.com>
2021-10-17 19:30:43 +0700

    refactor: desktop router

--- lib/desktop/routes/desktop_route_names.dart
+++ lib/desktop/routes/desktop_route_names.dart
@@ -1,6 +1,6 @@
 class DesktopRoutes {
-  static const String DESKTOP_EDIT_PROFILE = 'desktop_home';
-  static const String DESKTOP_USER_PROFILE = 'desktop_profile';
+  static const String DESKTOP_EDIT_PROFILE = 'desktop_edit_profile';
+  static const String DESKTOP_USER_PROFILE = 'desktop_user_profile';
   static const String DESKTOP_LOGIN = 'desktop_login';
-  static const String DESKTOP_MY_PROFILE = 'desktop_main';
+  static const String DESKTOP_MY_PROFILE = 'desktop_my_profile';
 }
