4c3c3452f6e605f432c45c77fa343a28eb0fc72e
Nitesh Kumar <niteshk@geekyants.com>
2022-01-21 16:13:10 +0530

    Used flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@
 import 'dart:async';
 
 import 'package:at_wavi_app/app.dart';
+import 'package:at_wavi_app/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runZoned<Future<void>>(() async {
+    await MixedConstants.load();
     // ignore: unawaited_futures
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
